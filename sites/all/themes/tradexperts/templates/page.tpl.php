3636a9107e08331d3c9809b29d3cc7244078b903
ksis <ksis@mail.com>
2014-12-02 18:06:58 +0200

    Created module for manage metadata and titles

--- sites/all/themes/tradexperts/templates/page.tpl.php
+++ sites/all/themes/tradexperts/templates/page.tpl.php
@@ -52,17 +52,17 @@
           <!-- <?php print render($page['highlighted']); ?> -->
           <!-- <?php print $breadcrumb; ?> -->
           <!-- <a id="main-content"></a> -->
-          <!-- <?php print render($title_prefix); ?> -->
-          <!-- <?php if ($title): ?> -->
-            <!-- <h1 class="page__title title" id="page-title"><?php print $title; ?></h1> -->
-          <!-- <?php endif; ?> -->
-          <!-- <?php print render($title_suffix); ?> -->
+          <?php print render($title_prefix); ?>
+          <?php if ($title && !$is_front): ?>
+            <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
+          <?php endif; ?>
+          <?php print render($title_suffix); ?>
           <?php print $messages; ?>
           <?php print render($tabs); ?>
           <!-- <?php print render($page['help']); ?> -->
-          <!-- <?php if ($action_links): ?> -->
-            <!-- <ul class="action-links"><?php print render($action_links); ?></ul> -->
-          <!-- <?php endif; ?> -->
+          <?php if ($action_links): ?>
+            <ul class="action-links"><?php print render($action_links); ?></ul>
+          <?php endif; ?>
           <?php print render($page['content']); ?>
           <!-- <?php print $feed_icons; ?> -->
         </div>
