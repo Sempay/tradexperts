c5b4154d9f4146332d4e3783cac0d6eeb9e51553
Fess <san.skif@mail.ru>
2014-12-05 12:18:14 +0200

    correction in the module and its modernization

--- sites/all/modules/custom/tradexperts_categories/templates/tradexperts-categories.tpl.php
+++ sites/all/modules/custom/tradexperts_categories/templates/tradexperts-categories.tpl.php
@@ -1,7 +1,7 @@
-<div class="category-body-content">
-<div class="image-category">
+<div class="categories-body-content">
+<div class="image-categories">
   <?php foreach ($select as $key => $value) { ?>
-  <div class="category-news-picture">
+  <div class="categories-news-picture">
     <?php
       $ways = array(
       	'style_name' => '100x52sc', 
@@ -10,7 +10,7 @@
       print theme('image_style', $ways);
      	?>
       </div>
-    <div class="category-news-title">
+    <div class="categories-news-title">
       <?php print l($value->title, 'node/' . $value->nid); ?>
     </div>
     <br>
