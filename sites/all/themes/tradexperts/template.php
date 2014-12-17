3756ffab84aab37dc6b3145a710a14708c588ec7
Fess <san.skif@mail.ru>
2014-12-17 12:39:33 +0200

    now must work

--- sites/all/themes/tradexperts/template.php
+++ sites/all/themes/tradexperts/template.php
@@ -75,7 +75,9 @@ function tradexperts_preprocess_page(&$variables, $hook) {
       array_unshift($breadcrumbs, l(t('Home'), '<front>'));
       drupal_set_breadcrumb($breadcrumbs);
     }
+  }
   if(drupal_is_front_page()) {
+    // Уберём default_message с главной
     unset($variables['page']['content']['system_main']['default_message']);
   }
 }
