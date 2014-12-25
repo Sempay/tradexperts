453fafcfceb32f68e318aa37bf4a8f9339f77848
ksis <ksis@mail.com>
2014-12-25 11:15:16 +0200

    Corrected site for some screens

--- sites/all/themes/tradexperts/templates/page.tpl.php
+++ sites/all/themes/tradexperts/templates/page.tpl.php
@@ -14,25 +14,27 @@
     <div class="full-line"></div>
     <div class="steps-bg"></div>
     <div class="header-content clearfix">
-      <?php if ($logo): ?>
-        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
-      <?php endif; ?>
+      <div class="header-center clearfix">
+        <?php if ($logo): ?>
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
+        <?php endif; ?>
 
-      <?php if ($site_name || $site_slogan): ?>
-        <div class="header__name-and-slogan" id="name-and-slogan">
-          <?php if ($site_name): ?>
-            <h1 class="header__site-name" id="site-name">
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
-            </h1>
-          <?php endif; ?>
+        <?php if ($site_name || $site_slogan): ?>
+          <div class="header__name-and-slogan" id="name-and-slogan">
+            <?php if ($site_name): ?>
+              <h1 class="header__site-name" id="site-name">
+                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
+              </h1>
+            <?php endif; ?>
 
-          <?php if ($site_slogan): ?>
-            <div class="header__site-slogan" id="site-slogan"><?php print $site_slogan; ?></div>
-          <?php endif; ?>
-        </div>
-      <?php endif; ?>
+            <?php if ($site_slogan): ?>
+              <div class="header__site-slogan" id="site-slogan"><?php print $site_slogan; ?></div>
+            <?php endif; ?>
+          </div>
+        <?php endif; ?>
 
-      <?php print render($page['header']); ?>
+        <?php print render($page['header']); ?>
+      </div>
     </div>
     <div class="steps-bg"></div>
     <div class="full-line"></div>
